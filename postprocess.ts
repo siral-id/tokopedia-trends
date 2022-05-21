6fbaca08e32817563ba160b0577b5ebf571a2536
Kelvin Desman <kelvindesman@Kelvins-iMac.local>
2022-05-21 12:24:13 +0700

    fix isotimestamp

--- postprocess.ts
+++ postprocess.ts
@@ -28,6 +28,7 @@ interface trend {
   image: string;
   count: number;
   source: string;
+  timestamp: string;
 }
 
 const filename = Deno.args[0]; // Same name as downloaded_filename
@@ -37,11 +38,14 @@ const keywords = response["data"]["popular_keywords"]["keywords"];
 
 const currentTrends: trend[] = keywords.map(
   ({ keyword, image_url, product_count }) => {
+    const timestamp = new Date().toISOString();
+
     return {
       keyword,
       count: product_count,
       image: image_url,
       source: "TOKOPEDIA",
+      timestamp,
     };
   },
 );
@@ -49,6 +53,6 @@ const currentTrends: trend[] = keywords.map(
 const output = `tokopedia_trends.json`;
 const existingTrends = await readJSON(output);
 
-const trends = existingTrends.concat(currentTrends)
+const trends = existingTrends.concat(currentTrends);
 
 await writeJSON(output, trends);
