ed3ee02ee3057612d95b8675ddca77b5df578f81
Kelvin Desman <kelvindesman@Kelvins-iMac.local>
2022-06-09 21:09:07 +0700

    bugfix: fix cli error

--- cli.ts
+++ cli.ts
@@ -1,12 +1,15 @@
-import { ICreateTrend, pull, serializeResponse, setupOctokit, upload } from "./mod.ts";
+import { pull } from "./mod.ts";
+import {
+  ICreateTrend,
+  setupOctokit,
+  upload,
+} from "https://raw.githubusercontent.com/siral-id/core/main/mod.ts";
 
 const ghToken = Deno.env.get("GH_TOKEN");
 const octokit = setupOctokit(ghToken);
 
 const response = await pull();
 
-const serializedData = serializeResponse(response);
-
-await upload<ICreateTrend[]>(octokit, serializedData, "WRITE_TOKOPEDIA_TRENDS");
+await upload<ICreateTrend[]>(octokit, response, "WRITE_TOKOPEDIA_TRENDS");
 
 Deno.exit();
